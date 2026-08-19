8e435350e52c9ac33221fea48aa48858c06fbb08
purreo <saviorofthesun@gmail.com>
2026-08-19 08:11:36 -0400

    update manifest to hide key

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,10 +1,14 @@
 import { defineManifest } from '@crxjs/vite-plugin';
+import { loadEnv } from 'vite';
+
+const currentDirectory = (globalThis as any).process?.cwd?.() || '';
+const env = loadEnv('', currentDirectory, '');
 
 export default defineManifest({
   manifest_version: 3,
   name: 'Neetcode Spaced Repetition',
   version: '1.0.0',
-  key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApQl7yRak+klCSi5Sg629DrYjOk0+WaVo3Ti5XpGHdkYHnl2+C3A0Ff6GbqxKYl3kHn0UzbDbo8Y2VipuWAQ2XDWAl4kF6uFKxgBUJUjGoNLhyJZ3na/QCkm8CahWnKIG+l0HwRGmwjM04QxZzqPurKINQAF06b7ThK7dJG1uwNpXNcQYu9lNtuMU79k6aRgfUWT0Eu745u80RnTA+hQdbkImZLotns0u/cnMOEWNRg2SSOFRvF7ed3EuY5NYQQ7rLp1/cMTxJBq/y+2CaoIZzxPc6VAPLh/WV71DZJj2GQDZUT3xA34L5UQ2EKHu8DoewFDviJg3ODEmhFhH08PZawIDAQAB',
+  key: env.CRX_MANIFEST_KEY,
   description: 'Automates spaced repetition for neetcode.io',
   icons: {
     '16': 'icon_16.png',
